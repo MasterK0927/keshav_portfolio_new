abd4e0aa1c8151da2cf15593e3eb4340e0fa9998
MasterK0927 <btech10189.22@bitmesra.ac.in>
2024-05-14 13:45:39 +0530

    first commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'masterK0927',
+  devUsername: "masterK0927",
+  github: 'https://github.com/masterK0927',
+  facebook: '',
+  linkedIn: 'https://www.linkedin.com/in/keshav-agarwal-0927',
+  twitter: 'https://twitter.com/masterK0927',
+  stackOverflow: 'https://stackoverflow.com/users/25030479/keshav-agarwal',
+  leetcode: "https://leetcode.com/MasterK0927/",
+  resume: "https://drive.google.com/file/d/1uQjvp9kt_bx78JBg_LF9_NKZRh2k8_h0/view",
+  skills: ['React', 'NextJS', 'Redux', 'Express', 'NextJS', 'MySql', 'MongoDB', 'ThreeJS','Firebase','Deep Learning', 'Generative AI','Docker', 'AWS'],
+  timezone: '+8'
+};
